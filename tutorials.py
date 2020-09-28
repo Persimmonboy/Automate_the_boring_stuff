5472672cf56491f2ac5ff937c268e9205360c134
Persimmonboy <shakhbazianartem@gmail.com>
2020-09-28 17:14:12 +0300

    first commit

--- tutorials.py
+++ tutorials.py
@@ -2,3 +2,4 @@ import pprint
 theBoard = {'low-L': ' ', 'low-M': ' ', 'low-R': ' ', 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ', 'top-L': ' ', 'top-M': ' ', 'top-R': ' '}
 
 pprint.pprint(theBoard)
+print('new comment')
